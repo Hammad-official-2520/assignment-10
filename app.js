50735f04f3a1b6f824b2c7786d7db758b1bdce8f
Hammad-official-2520 <106146562+Hammad-official-2520@users.noreply.github.com>
2022-07-15 07:47:50 +0500

    Update app.js
    assignment almost done

---
+++ app.js
@@ -0,0 +1,92 @@
+// function currentTime(){
+//     document.write(new Date())  
+// }
+// currentTime();
+
+// function name(a,b){
+//     var a = prompt("Enter First Name")
+//     var b = prompt("Enter Last Name")
+//     return a + b;
+// }
+// alert(name())
+
+// function sum(a,b){
+//     var a = +prompt("Enter First Number");
+//     var b = +prompt("Enter Second Number");
+//     return a + b
+// }
+// alert(sum());
+
+// function calculator(a,b,c){
+//     var a = +prompt("Enter First Number");
+//     var b = +prompt("Enter Second Number");
+//     var c =  prompt("Enter Operator");
+//     if(c === "+"){
+//       return  a + b
+//     }else if(c === "*"){
+//         return    a * b
+//     }else if(c === "-"){
+//         return   a - b
+//     }else if(c === "/"){
+//         return  a / b
+//   }
+// }
+// alert(calculator())
+
+// function square(){
+//     var a = +prompt("Enter a Number");
+//     return a * a
+// }
+// alert(square())
+
+
+//  var num = parseInt(prompt('Enter a positive integer: '));
+
+// if (num < 0) {
+//     alert('Error! Factorial for negative number does not exist.');
+// }
+// else if (num === 0) {
+//     alert(`The factorial of ${number} is 1`);
+// }
+// else {
+//     let fact = 1;
+//     for (i = 1; i <= num; i++) {
+//         fact *= i;
+//     }
+//     alert(`The factorial of ${num} is ${fact}.`);
+// }
+
+
+// /////////////////////////////////////Ye question puchna h sir se //////////////////////////////
+// function loop(){
+//    var a = +prompt("Enter starting number");
+//    var b = +prompt("Enter ending number");
+//     for(i = a; i < b; --i){
+//         return i
+//     }
+// }
+// alert(loop());
+
+
+// var a = +prompt("Enter height of triangle");
+// var b = +prompt("Enter width of triangle");
+// function area(a,b){
+//     document.write(a * b);
+// }
+// area(a,b);
+// var a = "the quick brown fox";
+// var b = a.split(" ");
+// console.log(b);
+// var c = b.pop();
+// console.log(c)
+// function calc(a){
+//     document.write(2*3.14*a);
+// }
+
+// var a =+prompt("Enter the radius of circle");
+// calc(a);
+// function calc(a){
+//     document.write(3.14*(a*a));
+// }
+// var a =+prompt("Enter the radius of circle");
+//  calc(a);
